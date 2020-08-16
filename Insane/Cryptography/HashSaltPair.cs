5a4780089f6ae5b2625575dda346c798a7573b3c
_megamanx_ <megamanx@outlook.com>
2020-08-16 18:55:48 -0500

    Constructor for HashSaltPair class

--- Insane/Cryptography/HashSaltPair.cs
+++ Insane/Cryptography/HashSaltPair.cs
@@ -8,6 +8,12 @@ namespace Insane.Cryptography
 {
     public class HashSaltPair
     {
+        public HashSaltPair(string hash, string salt)
+        {
+            Hash = hash;
+            Salt = salt;
+        }
+
         public String Hash { get; set; } = null!;
         public String Salt { get; set; } = null!;
     }
