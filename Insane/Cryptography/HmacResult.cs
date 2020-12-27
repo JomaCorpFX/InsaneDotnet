d60357bbd9fbbdf9e6ec8d3ac425ed088d9bc117
Joma <megamanx@outlook.com>
2020-12-27 03:49:49 -0500

    Argon2 + Scrypt

---
+++ Insane/Cryptography/HmacResult.cs
@@ -0,0 +1,36 @@
+﻿using Insane.Exceptions;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json;
+
+namespace Insane.Cryptography
+{
+    public class HmacResult
+    {
+        public string Hash { get; set; } = null!;
+        public string Key { get; set; } = null!;
+        public HashAlgorithm Algorithm { get; set; } = HashAlgorithm.Sha512;
+
+        public byte[] RawKey { get { return HashManager.FromBase64(Key); } }
+        public byte[] RawHash { get { return HashManager.FromBase64(Hash); } }
+
+        public HmacResult(string hash, string key, HashAlgorithm algorithm)
+        {
+            Hash = hash;
+            Key = key;
+            Algorithm = algorithm;
+        }
+
+        public static HmacResult Deserialize(string json)
+        {
+            HmacResult? obj = JsonSerializer.Deserialize<HmacResult>(json);
+            return obj == null ? throw new DeserializeException(typeof(HmacResult)) : obj;
+        }
+
+        public string Serialize()
+        {
+            return JsonSerializer.Serialize(this, new JsonSerializerOptions { WriteIndented = false });
+        }
+    }
+}
