5114c015a6bef34c3d10e625aad94eb0726e4d17
José Espinoza Bone <megamanx@outlook.com>
2022-02-17 16:24:54 -0500

    Added CoreDbContextFactory generator

--- Insane.WebApiLiveTests/EntityFrameworkCore/Factory/IdentityOracleDbContextFactory.cs
+++
@@ -1,8 +0,0 @@
-﻿using Insane.WebApiLiveTests.EntityFrameworkCore.Context;
-
-namespace Insane.WebApiLiveTests.EntityFrameworkCore.Factory
-{
-    public class IdentityOracleDbContextFactory : IdentityCoreDbContextFactoryBase<IdentityOracleDbContext>
-    {
-    }
-}
